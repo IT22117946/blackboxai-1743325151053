d0da06efbdf99666dcd55ab93358b944dbe399ce
IT22117946 <127727856+IT22117946@users.noreply.github.com>
2025-03-30 14:32:50 +0530

    Add src/main/java/com/example/skillshare/model/Comment.java 1743325370070

---
+++ src/main/java/com/example/skillshare/model/Comment.java
@@ -0,0 +1,29 @@
+package com.example.skillshare.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
