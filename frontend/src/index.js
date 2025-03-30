c76cb1de58874539e1b72e08533b4f24c71c7238
IT22117946 <127727856+IT22117946@users.noreply.github.com>
2025-03-30 14:31:17 +0530

    Add frontend/src/index.js 1743325277363

---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import axios from 'axios';
+
+// Set base URL for API requests
+axios.defaults.baseURL = 'http://localhost:8081/api';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
