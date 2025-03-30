252154070554a091c7fcf17d063d6e4154a2708e
IT22117946 <127727856+IT22117946@users.noreply.github.com>
2025-03-30 14:32:52 +0530

    Add src/main/java/com/example/skillshare/model/Post.java 1743325371656

---
+++ src/main/java/com/example/skillshare/model/Post.java
@@ -0,0 +1,34 @@
+package com.example.skillshare.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ElementCollection
+    @CollectionTable(name = "post_media", joinColumns = @JoinColumn(name = "post_id"))
+    @Column(name = "media_url")
+    private List<String> mediaUrls;
+
+    private int likeCount = 0;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
+}
